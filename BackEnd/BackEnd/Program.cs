8b0c457cd566784727bd5c3a16e3c22aa8347389
Shams A <shamsatia96@gmail.com>
2025-08-05 23:31:05 +0300

    add gitignore

--- BackEnd/BackEnd/Program.cs
+++ BackEnd/BackEnd/Program.cs
@@ -26,15 +26,17 @@ builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
     });
 
 builder.Services.AddAuthorization();
-
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 var app = builder.Build();
 
 if (app.Environment.IsDevelopment())
 {
     app.UseDeveloperExceptionPage();
 }
-
-app.UseHttpsRedirection();
+app.UseSwagger();
+app.UseSwaggerUI();
+//app.UseHttpsRedirection();
 app.UseAuthentication();
 app.UseAuthorization();
 
